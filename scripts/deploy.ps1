6bbe73efdc2991d3895a85cc70c459e5e5f935b7
Matt Mazzola <mattmazzola@live.com>
2023-01-29 14:09:44 -0800

    Add .gitattributes and .editorconfig

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -26,6 +26,7 @@ Write-Step "Get ENV Vars from $envFilePath"
 $databaseConnectionString = Get-EnvVarFromFile -envFilePath $envFilePath -variableName 'DATABASE_URL'
 $shadowDatabaseConnectionString = Get-EnvVarFromFile -envFilePath $envFilePath -variableName 'SHADOW_DATABASE_URL'
 $storageConnectionString = $(az storage account show-connection-string -g $sharedResourceGroupName -n $sharedResourceNames.storageAccount --query "connectionString" -o tsv)
+# $storageQueueName = $(az storage queue list --connection-string $storageConnectionString --query "[].name" -o tsv)
 
 Write-Step "Fetch params from Azure"
 $containerAppsEnvResourceId = $(az containerapp env show -g $sharedResourceGroupName -n $sharedResourceNames.containerAppsEnv --query "id" -o tsv)
@@ -46,6 +47,7 @@ $data = [ordered]@{
   "databaseConnectionString"       = "$($databaseConnectionString.Substring(0, 15))..."
   "shadowDatabaseConnectionString" = "$($shadowDatabaseConnectionString.Substring(0, 15))..."
   "storageConnectionString"        = "$($storageConnectionString.Substring(0, 15))..."
+  "storageQueueName"               = $storageQueueName
 
   "nodeProcessorImageName"         = $nodeProcessorImageName
   "clientImageName"                = $clientImageName
