c00273c00d6a0ffb0d3687568c07f40dab84ac87
Matt Mazzola <mattmazzola@live.com>
2023-02-10 23:05:32 -0800

    Add steps as comments

--- services/dotnet-processor/Program.cs
+++ services/dotnet-processor/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// 1. Load Environment Variables
+// 2. Validate Environment Variables
+// 3. Connect to Database
+// 4. Connect to Storage Queue
+// 5. Read Messages from Queue
+// 6. Dequeue Message
+// 7. Read Items from Database
+// 8. Compute Result
+// 9. Add Result Item to Database
+// 10. Exit
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
