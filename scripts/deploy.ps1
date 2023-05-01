a4369f9c667e18077ddbdf4c7c9b98b99082b444
Matt Mazzola <mattmazzola@live.com>
2023-04-30 22:16:27 -0700

    Update deploy script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -27,8 +27,6 @@ $batchProcessorResourceGroupName = "batch-processor"
 $sharedResourceNames = Get-ResourceNames $sharedResourceGroupName $sharedRgString
 $batchProcessorResourceNames = Get-LocalResourceNames $batchProcessorResourceGroupName 'unused'
 
-echo $sharedResourceNames
-
 Write-Step "Create Resource Group: $batchProcessorResourceGroupName"
 az group create -l $resourceGroupLocation -g $batchProcessorResourceGroupName --query name -o tsv
 
@@ -39,25 +37,25 @@ $databaseConnectionString = Get-EnvVarFromFile -envFilePath $envFilePath -variab
 $storageConnectionString = $(az storage account show-connection-string -g $sharedResourceGroupName -n $($sharedResourceNames.storageAccount) --query "connectionString" -o tsv)
 $nodeStorageQueueName = $(az storage queue list --connection-string $storageConnectionString --query "[0].name" -o tsv)
 $pythonStorageQueueName = $(az storage queue list --connection-string $storageConnectionString --query "[1].name" -o tsv)
-$serviceBusNamespaceConnectionString = $(az servicebus namespace authorization-rule keys list -g $sharedResourceGroupName --namespace-name $($sharedResourceNames.serviceBus) -n 'RootManageSharedAccessKey' --query 'primaryConnectionString' -o tsv)
+$serviceBusNamespaceConnectionString = $(az servicebus namespace authorization-rule keys list -g $sharedResourceGroupName --namespace-name $($sharedResourceNames.serviceBus) --name 'RootManageSharedAccessKey' --query 'primaryConnectionString' -o tsv)
 $serviceBusQueueName = $(az servicebus queue list -g $sharedResourceGroupName --namespace-name $($sharedResourceNames.serviceBus) --query '[0].name' -o tsv)
 
 Write-Step "Fetch params from Azure"
 $sharedResourceVars = Get-SharedResourceDeploymentVars $sharedResourceGroupName $sharedRgString
 
-$nodeProcessorContainerName = ${batchProcessorResourceNames.containerAppNodeStorageQueue}
+$nodeProcessorContainerName = $($batchProcessorResourceNames.containerAppNodeStorageQueue)
 $nodeProcessorImageTag = $(Get-Date -Format "yyyyMMddhhmm")
 $nodeProcessorImageName = "$($sharedResourceVars.registryUrl)/${nodeProcessorContainerName}:${nodeProcessorImageTag}"
 
-$nodeSbProcessorContainerName = ${batchProcessorResourceNames.containerAppNodeServiceBusQueue}
+$nodeSbProcessorContainerName = $($batchProcessorResourceNames.containerAppNodeServiceBusQueue)
 $nodeSbProcessorImageTag = $(Get-Date -Format "yyyyMMddhhmm")
 $nodeSbProcessorImageName = "$($sharedResourceVars.registryUrl)/${nodeSbProcessorContainerName}:${nodeSbProcessorImageTag}"
 
-$pythonProcessorContainerName = ${batchProcessorResourceNames.containerAppPythonStorageQueue}
+$pythonProcessorContainerName = $($batchProcessorResourceNames.containerAppPythonStorageQueue)
 $pythonProcessorImageTag = $(Get-Date -Format "yyyyMMddhhmm")
 $pythonProcessorImageName = "$($sharedResourceVars.registryUrl)/${pythonProcessorContainerName}:${pythonProcessorImageTag}"
 
-$clientContainerName = ${batchProcessorResourceNames.containerAppClient}
+$clientContainerName = $($batchProcessorResourceNames.containerAppClient)
 $clientImageTag = $(Get-Date -Format "yyyyMMddhhmm")
 $clientImageName = "$($sharedResourceVars.registryUrl)/${clientContainerName}:${clientImageTag}"
 
